c410ce0eb8e458b6aa50908096bc9933ec98e90b
Abyss363 <fortuneodo@gmail.com>
2024-09-20 15:03:05 +0100

    Fixed infinite menu selection issue (Probably)

--- MainMenu.cs
+++ MainMenu.cs
@@ -8,40 +8,42 @@ public class MainMenu
 
     public MainMenu()
     {
+        //Obviously, the actual menu
         Console.WriteLine("-----------------------WELCOME TO THE LOST WORLDS-----------------------");
         Console.WriteLine("\n\n\n\n");
 
-        Console.WriteLine("********** 1. New game   **********");
-        Console.WriteLine("********** 2. Continue   **********");
-        Console.WriteLine("********** 3. Credits    **********");
-        Console.WriteLine("********** 4. Exit       **********");
+        Console.WriteLine("********************** 1. New game   **********************");
+        Console.WriteLine("********************** 2. Continue   **********************");
+        Console.WriteLine("********************** 3. Credits    **********************");
+        Console.WriteLine("********************** 4. Exit       **********************");
 
         Console.WriteLine("Enter an Option. . . . . . . . . . .");
 
 
-
-        while (validEntry == false)
+        do
         {
-            input = Console.ReadLine();
-
-            if (!string.IsNullOrEmpty(input) && int.TryParse(input, out int value))
+            while (validEntry == false)
             {
-                intInput = Convert.ToInt32(input);
-                if (intInput < 1 || intInput > 4)
+                input = Console.ReadLine();
+
+                //Checking if the player input is valid
+                if (!string.IsNullOrEmpty(input) && int.TryParse(input, out int value))
                 {
-                    Console.WriteLine("Invalid option");
+                    intInput = Convert.ToInt32(input);
+                    if (intInput < 1 || intInput > 4)
+                    {
+                        Console.WriteLine("Invalid option");
+                    }
+                    else { validEntry = true; }
                 }
-                else { validEntry = true; }
+                else
+                {
+                    Console.WriteLine("Invalid Input. Please enter a number corresponding to an option to proceed");
+                }
             }
-            else
-            {
-                Console.WriteLine("Invalid Input. Please enter a number corresponding to an option to proceed");
-            }
-        }
-        validEntry = false;
+            validEntry = false;
 
-        while (true)
-        {
+            //Menu leads to other parts of the program
             switch (intInput)
             {
                 case 1:
@@ -59,6 +61,7 @@ public class MainMenu
                     return;
             }
         }
+        while (true);
     }
 
 }
